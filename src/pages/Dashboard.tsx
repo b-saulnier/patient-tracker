ad529b219ca177cb52d09b8f6867486cd51130e2
Brandon  Saulnier <124940268+b-saulnier@users.noreply.github.com>
2023-12-05 14:08:44 -0500

    preservice mock data still works

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -23,9 +23,12 @@ import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import { Link } from 'react-router-dom';
 import { MainLayout } from '../components/MainLayout';
 import { Patient } from '../models/Patient';
+import { Event } from '../models/Event';
 import { PrescriptionBox, Prescription } from '../components/PrescriptionBox'
 import { supabase } from '../components/AuthBuilder';
 import { DatabaseService } from '../services/Patient'
+import { EventsSchedule } from '../components/EventsSchedule';
+import { CircularProgress } from '@mui/material';
 const drawerWidth = 200;
 
 interface Props {
@@ -42,12 +45,46 @@ interface Links {
     icon: React.ReactElement
 }
 
+const sampleEvents = [
+    {
+        id: 1,
+        patient_id: 1,
+        start_time: '2021-10-01T10:00:00',
+        end_time: '2021-10-01T11:00:00',
+        type: 'Appointment',
+        doctor_id: 1,
+        first_name: 'Sarah',
+        last_name: 'Surgeon',
+    } as Event,
+    {
+        id: 2,
+        patient_id: 2,
+        start_time: '2021-10-02T10:00:00',
+        end_time: '2021-10-02T11:00:00',
+        type: 'Random',
+        doctor_id: 2,
+        first_name: 'Daniel',
+        last_name: 'Doctor',
+    } as Event,
+    {
+        id: 3,
+        patient_id: 3,
+        start_time: '2021-10-03T10:00:00',
+        end_time: '2021-10-03T13:00:00',
+        type: 'Misc.',
+        doctor_id: 3,
+        first_name: 'Phyllis',
+        last_name: 'Physician',
+    } as Event,
+];
+
 export function DashboardPage(props: Props) {
     const { window } = props;
     const [mobileOpen, setMobileOpen] = React.useState(false);
     const [isRendered, setIsRendered] = React.useState(false);
     const [isDoctor, setIsDoctor] = React.useState(false);
     const [userPrescriptions, setUserPrescriptions] = React.useState([]);
+    const [events, setEvents] = React.useState<Event[]>([]);
 
     React.useEffect(() => {
         console.log('hello????');
@@ -71,12 +108,44 @@ export function DashboardPage(props: Props) {
         fetchData();
         setIsRendered(true);
     }, []);
+
+    React.useEffect(() => {
+        setEvents(sampleEvents);
+        console.log("set events");
+    }, []);
     
-    return !isRendered ? 
-            <div>Rendering!</div> :
-            isDoctor ? 
-                <div>I am a doctor!</div> :
-                <Box sx={{ margin:'20px', display:'flex', width:'100%'}}>
-                    <PrescriptionBox prescriptions={ userPrescriptions }/>
-                </Box>;
+    // return !isRendered ? 
+    //         <div>Rendering!</div> :
+    //         isDoctor ? 
+    //             <div>I am a doctor!</div> :
+    //             <>
+    //                 <Box sx={{ margin:'20px', display:'flex', width:'100%'}}>
+    //                     <PrescriptionBox prescriptions={ userPrescriptions }/>
+    //                 </Box>;
+
+    //                 <EventsSchedule events={events}/>
+    //             </>
+
+    return <>
+        <div className="p-8 h-screen w-full">
+            <div>
+                <h1 className='text-2xl font-semibold'>Dashboard</h1>
+                <div className='border-b border-gray-300 w-full mb-8 mt-4' />
+            </div>
+            {!isRendered ? 
+                <Box sx={{ display: 'flex' }}>
+                    <CircularProgress />
+                </Box>
+                : 
+                <>
+                    <Box sx={{ margin:'20px', display:'flex', width:'100%'}}>
+                        <PrescriptionBox prescriptions={ userPrescriptions }/>
+                    </Box>
+
+                    <EventsSchedule events={events!}/>
+                </>
+            }
+        </div>;
+    </>
+                
 }
